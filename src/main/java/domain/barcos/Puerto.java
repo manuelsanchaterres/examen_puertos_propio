600e9c3f0647e3e46c823229a5d3704f9d93ce34
manuelsanchaterres <manuelsanchaterres@gmail.com>
2024-04-28 12:36:59 +0200

    método buscarAmarre de clase Puerto casi acabado, falta resolver el bug de ConcurrentModificationException

--- src/main/java/domain/barcos/Puerto.java
+++ src/main/java/domain/barcos/Puerto.java
@@ -31,18 +31,23 @@ public class Puerto {
         // PANTALAN UNO
         Amarre amarreUno = new Amarre(110,true, new Comercial("4567", "Tsunami", 110, "Carga Textil"));
         Amarre amarreDos = new Amarre(100,true, new Comercial("2345", "Triton", 30, "Teléfonos"));
+        // AMARRE COMERCIAL VACÍO
+        Amarre amarreTres = new Amarre(100,false, null);
         ArrayList<Amarre> amarresUno = new ArrayList<Amarre>();
         amarresUno.add(amarreUno);
         amarresUno.add(amarreDos);
+        amarresUno.add(amarreTres);
         Pantalan pantalanUno = new Pantalan("Comercial", amarresUno);
 
         // PANTALAN DOS
 
-        Amarre amarreTres = new Amarre(50,true, new Recreo("5678", "Berberecho", 4, "Permiso internacional"));
-        Amarre amarreCuatro = new Amarre(300,true, new Recreo("6789", "Mejillon", 3, "Permiso recreativo"));
+        Amarre amarreCuatro = new Amarre(50,true, new Recreo("5678", "Berberecho", 4, "Permiso internacional"));
+        Amarre amarreCinco = new Amarre(300,true, new Recreo("6789", "Mejillon", 3, "Permiso recreativo"));
+        Amarre amarreSeis = new Amarre(100,false, null);
         ArrayList<Amarre> amarresDos = new ArrayList<Amarre>();
-        amarresDos.add(amarreTres);
         amarresDos.add(amarreCuatro);
+        amarresDos.add(amarreCinco);
+        amarresDos.add(amarreSeis);
         Pantalan pantalanDos = new Pantalan("Recreo", amarresDos);
 
         //TODO ¿PORQUE LA ID AUTOINCREMENTAL DE AMARRRE SÓLO SE MANTIENE DENTRO DE LA MISMA ESTANCIA DE PANTALAN Y NO SI CREAMOS NUEVAS ESTANCIAS DE PANTALAN ?
@@ -51,5 +56,37 @@ public class Puerto {
 
         listadoPantalanes.add(pantalanUno);
         listadoPantalanes.add(pantalanDos);
+
+    }
+
+    public static void buscarAmarre (ArrayList<Barco> barcosEntrantes, ArrayList<Pantalan> listadoPantalanes) {
+
+        // BUSCAR COINCIDENCIA TIPO DE BARCO Y TIPO DE PANTALÁN
+        System.out.println(barcosEntrantes);
+        for (Pantalan pantalan: listadoPantalanes) {
+
+            for (Barco barco: barcosEntrantes) {
+
+                if (barco.getClass().getSimpleName().equals(pantalan.tipo)) {
+
+                    for (Amarre amarre : pantalan.listaAmarres) {
+                        /* SI EL TAMAÑO DEL AMARRE ES MAYOR O IGUAL AL TAMAÑO DEL BARCO Y EL AMARRE NO ESTÁ OCUPADO,
+                        ENTONCES ASIGNAMOS EL BARCO AL AMARRE  Y SACAMOS EL BARCO DE LISTADO BARCOSENTRANTES */
+
+                        if (amarre.tamaño >= barco.tamaño && !amarre.ocupado) {
+
+                            amarre.barco = barco;
+                            amarre.ocupado = true;
+                            /* TODO CONSULTAR ESTA SOLUCIÓN DE STACKOVERFLOW PARA RESOLVER LA EXCEPCIÓN ConcurrentModificationException
+                            AL REMOVER UN ITEM DEL ARRAYLIST MIENTRAS ITERAS SOBRE ÉL*/
+                            //https://stackoverflow.com/questions/223918/iterating-through-a-collection-avoiding-concurrentmodificationexception-when-re
+                            barcosEntrantes.remove(barco);
+                            System.out.println(barcosEntrantes);
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,9 +8,10 @@ public class Main {
     public static void main(String[] args) {
 
         Puerto puerto = new Puerto();
-//        Barco.cargarBarcosEntrantes(puerto.barcosEntrantes);
+        Barco.cargarBarcosEntrantes(puerto.barcosEntrantes);
 //        System.out.println(puerto.barcosEntrantes);
         Puerto.simular(puerto.listadoPantalanes);
-        Puerto.verAmarre(puerto.listadoPantalanes);
+//        Puerto.verAmarre(puerto.listadoPantalanes);
+        Puerto.buscarAmarre(puerto.barcosEntrantes, puerto.listadoPantalanes);
     }
 }
